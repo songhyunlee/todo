f3ce70d6ec70ba818a08f13f0a8b7716b9e462ea
Songhyun Lee <empressivlee@gmail.com>
2016-07-22 12:44:41 -0700

    Angular controllers

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var app = angular.module('todo', []);
 
-app.controller('homeController', function() {
+app.controller('homeController', home);
+
+function home() {
   var vm = this;
   vm.message = "Hello world."
 })
